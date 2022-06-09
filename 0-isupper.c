6c2294691317b8b982011a7eb31f0671fdae4f2c
Christopher B. Jones <joneschristopher379@gmail.com>
2022-06-09 16:03:18 -0700

    op

---
+++ 0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ *isupper - check for uppercase letters
+ *
+ *return - (1) if uppercase or (0) if otherwise
+ *
+ **/
+
+int _isupper(int c)
+{
+	int a;
+
+	if (a == 'C')
+		putchar(1);
+	else
+		putchar(0);
+
+	return (0);
+}
